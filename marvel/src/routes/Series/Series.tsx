133611866e3977b2364a877225fc7529e18e4f03
Stephan-Neo <stepan.kazantsev.04@bk.ru>
2022-12-17 17:30:47 +0600

    feat: add debounce search

--- marvel/src/routes/Series/Series.tsx
+++ marvel/src/routes/Series/Series.tsx
@@ -1,20 +1,16 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { useTranslation } from 'react-i18next';
-import { SubmitHandler, useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import cardsStore from '../../stores/CardsStore';
-import { ApiLink } from '../../types/apiLink';
+import { useTranslation } from 'react-i18next';
+import { debounce } from 'lodash';
 import CardLayout from '../../components/Card';
+import cardsStore from '../../stores/CardsStore';
 import { getCardsV } from '../../api/Card';
+import { ApiLink } from '../../types/apiLink';
 import Error from '../../components/Error';
 
-type Inputs = {
-  example: string;
-};
 function Series(): ReactElement {
   const { t } = useTranslation();
-  const { register, handleSubmit } = useForm<Inputs>();
 
   const [offset, setOffset] = useState(0);
   const [startSearch, setStartSearch] = useState(0);
@@ -23,13 +19,23 @@ function Series(): ReactElement {
   const [countCards, setCountCards] = useState(0);
   const [error, setError] = useState<string>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    setSearch(data.example);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(e.target.value);
+  };
+
+  const debouncedSearch = debounce((data) => {
+    setSearch(data);
     setOffset(0);
     cardsStore.clearCards();
     setStartSearch(startSearch + 1);
     setCountCards(0);
-  };
+  }, 500);
+
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [debouncedSearch]);
 
   useEffect(() => {
     cardsStore.clearCards();
@@ -83,12 +89,11 @@ function Series(): ReactElement {
     <>
       {!error ? (
         <>
-          <Search onSubmit={handleSubmit(onSubmit)}>
-            <Input {...register('example')} type="text" />
-            <Button type="submit">
-              <p>{t('search')}</p>
-            </Button>
-          </Search>
+          <Input
+            type="text"
+            onChange={handleChange}
+            placeholder={`${t('search')}`}
+          />
           {isRemoteCards ? (
             <Wrapper>
               {cardsStore.cards.map((card) => (
@@ -115,37 +120,16 @@ const Wrapper = styled.div`
   grid-template-rows: repeat(2, 1fr);
 `;
 
-const Search = styled.form`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  padding: 20px 20px 0 30px;
-  justify-content: space-between;
-`;
-
 const Input = styled.input`
-  width: 100%;
   -webkit-box-shadow: 1px -1px 8px 6px rgba(34, 60, 80, 0.2);
   -moz-box-shadow: 1px -1px 8px 6px rgba(34, 60, 80, 0.2);
   box-shadow: 1px -1px 8px 6px rgba(34, 60, 80, 0.2);
-  padding: 10px;
-  margin-right: 20px;
+  padding: 20px;
+  margin: 30px 20px;
   font-size: 20px;
   ::placeholder {
     color: gray;
   }
-`;
-
-const Button = styled.button`
-  width: 200px;
-  text-align: center;
-  padding: 15px;
-  font-size: 20px;
-  font-weight: 700;
-  background: red;
-  -webkit-box-shadow: 1px -1px 8px 6px rgba(34, 60, 80, 0.2);
-  -moz-box-shadow: 1px -1px 8px 6px rgba(34, 60, 80, 0.2);
-  box-shadow: 1px -1px 8px 6px rgba(34, 60, 80, 0.2);
 `;
 
 export default observer(Series);
